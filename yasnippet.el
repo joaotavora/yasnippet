e025556fdf3a71c7d5fa105ab1d585cd241bf8ee
Zhang Chiyuan <pluskid@gmail.com>
2008-03-10 14:43:36 +0000

    release a new version(bugfix)

--- yasnippet.el
+++ yasnippet.el
@@ -3,7 +3,7 @@
 ;; Copyright 2008 pluskid
 ;; 
 ;; Author: pluskid <pluskid@gmail.com>
-;; Version: 0.2.0
+;; Version: 0.2.1
 ;; X-URL: http://code.google.com/p/yasnippet/
 
 ;; This file is free software; you can redistribute it and/or modify
@@ -89,7 +89,7 @@ mode will be listed under the menu \"yasnippet\".")
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Internal variables
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defvar yas/version "0.2.0")
+(defvar yas/version "0.2.1")
 
 (defvar yas/snippet-tables (make-hash-table)
   "A hash table of snippet tables corresponding to each major-mode.")
