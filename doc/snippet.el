56b8887538f0917bd278e9f15854c98f84c0a65a
João Távora <joaotavora@gmail.com>
2012-05-07 09:26:42 +0100

    scaffolding for a doc revamp

---
+++ doc/snippet.el
@@ -0,0 +1,12 @@
+(with-current-buffer "manual.org" )
+(dolist (file '("index.org"
+                "snippet-organization.org"
+                "snippet-expansion.org"
+                "snippet-development.org"
+                "snippet-menu.org"
+                "faq.org"
+                "changelog.org"))
+
+
+
+  )
