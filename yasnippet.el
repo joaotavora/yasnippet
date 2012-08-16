joaotavora/yasnippet

commit 71c8e0ea469c2992a31d17b0722ebdeb789fd1bf

(yas-choose-value): Accept strings as &rest argument

yasnippet.el
@@ -2749,10 +2749,16 @@ If found, the content of subexp group SUBEXP (default 0) is
           (match-string-no-properties grp str)
         str))))
 
-(defun yas-choose-value (possibilities)
-  "Prompt for a string in the list POSSIBILITIES and return it."
+(defun yas-choose-avalue (&rest possibilities)
+  "Prompt for a string in POSSIBILITIES and return it.
+
+The last element of POSSIBILITIES may be a list of strings."
   (unless (or yas-moving-away-p
               yas-modified-p)
+    (setq possibilities (nreverse possibilities))
+    (setq possibilities (if (listp (car possibilities))
+                            (append (reverse (car possibilities)) (rest possibilities))
+                                   possibilities))
     (some #'(lambda (fn)
               (funcall fn "Choose: " possibilities))
           yas-prompt-functions)))

commit 38c4b734c9b02c036818d5c99083c5ebdf704ece

Closes #285: fix typo introduced in 71c8e0ea469c2992a31d17b0722ebdeb789fd1bf

yasnippet.el
@@ -2773,7 +2773,7 @@ If found, the content of subexp group SUBEXP (default 0) is
           (match-string-no-properties grp str)
         str))))
 
-(defun yas-choose-avalue (&rest possibilities)
+(defun yas-choose-value (&rest possibilities)
   "Prompt for a string in POSSIBILITIES and return it.
 
 The last element of POSSIBILITIES may be a list of strings."