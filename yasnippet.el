901e53b78d3bd92c324d4394222c592d9bf4c4d9
Noam Postavsky <npostavs@users.sourceforge.net>
2016-04-14 21:52:36 -0400

    Only kill snippets for text length change
    of protection overlay.  Changes that keep the same length of text are
    probably harmless (e.g. just change of text properties).  It's possible
    that some harmful changes don't change text length, but the protection
    overlays are already unable to catch all important changes since they
    only cover 1 character each at the edges.
    
    * yasnippet.el (yas--on-protection-overlay-modification): Check length
    of text change to decide if change is harmful.  Fix docstring.

--- yasnippet.el
+++ yasnippet.el
@@ -3467,23 +3467,16 @@ Move the overlays, or create them if they do not exit."
              ;; (overlay-put ov 'evaporate t)
              (overlay-put ov 'modification-hooks '(yas--on-protection-overlay-modification)))))))
 
-(defvar yas--buffer-pre-modification)
-
-(defun yas--on-protection-overlay-modification (_overlay after? beg end &optional _length)
-  "Signals a snippet violation, then issues error.
-
-The error should be ignored in `debug-ignored-errors'"
+(defun yas--on-protection-overlay-modification (_overlay after? beg end &optional length)
+  "Commit the snippet if the protection overlay is being killed."
   (unless (or yas--inhibit-overlay-hooks
+              (not after?)
+              (/= length (- end beg)) ; deletion or insertion
               (yas--undo-in-progress))
-    (if after?
-        (unless (string= yas--buffer-pre-modification
-                         (buffer-substring-no-properties beg end))
-         (let ((snippets (yas--snippets-at-point)))
-           (yas--message 3 "Comitting snippets. Action would destroy a protection overlay.")
-           (cl-loop for snippet in snippets
-                    do (yas--commit-snippet snippet))))
-      (setq-local yas--buffer-pre-modification
-                  (buffer-substring-no-properties beg end)))))
+    (let ((snippets (yas--snippets-at-point)))
+      (yas--message 3 "Comitting snippets. Action would destroy a protection overlay.")
+      (cl-loop for snippet in snippets
+               do (yas--commit-snippet snippet)))))
 
 (add-to-list 'debug-ignored-errors "^Exit the snippet first!$")
 
